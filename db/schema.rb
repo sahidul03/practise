5012574b6d4823b09de4ce3895c6af85f13adcef
sahidul03 <sahidul03@gmail.com>
2015-06-29 22:48:29 +0600

    git ignore add

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150629063008) do
     t.string   "profile_photo"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "albums"
   end
 
 end
